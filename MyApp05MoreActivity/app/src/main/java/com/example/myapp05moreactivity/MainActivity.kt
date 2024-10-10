169d9faf6f1baf8e0e3f0098ca561c0c29c77cbf
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-10-10 15:27:13 +0200

    #5

--- MyApp05MoreActivity/app/src/main/java/com/example/myapp05moreactivity/MainActivity.kt
+++ MyApp05MoreActivity/app/src/main/java/com/example/myapp05moreactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp05moreactivity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSecond = findViewById<Button>(R.id.btnSecond)
+        val etNickname = findViewById<EditText>(R.id.etNickname)
+
+        btnSecond.setOnClickListener {
+            val nickname = etNickname.text.toString() // získáme text z edit textu pole
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("NICK_NAME", nickname)
+            startActivity(intent)
         }
+
+
+
     }
 }
---
+++ MyApp05MoreActivity/app/src/main/java/com/example/myapp05moreactivity/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapp05moreactivity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        val twInfo = findViewById<TextView>(R.id.twInfo)
+
+        //načtení dat z internetu
+        val nickname = intent.getStringExtra("NICK_NAME")
+        twInfo.text = nickname
+        twInfo.text = "data z první aktivity je: $nickname"
+
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
