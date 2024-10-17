25ddbb4528ae9e81fb1aebed20dbe410208f33ea
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-10-17 11:43:10 +0200

    #6

--- MyApp06ToastSnackbar/app/src/main/java/com/example/myapp06toastsnackbar/MainActivity.kt
+++ MyApp06ToastSnackbar/app/src/main/java/com/example/myapp06toastsnackbar/MainActivity.kt
@@ -8,32 +8,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapp06toastsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        //inicializace ViewBinding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // nastavení akce kliknutí na tlačítko
-        binding.btnShowToast.setOnClickListener {
-            val toast = Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show()
-            toast.show ()
-            
-
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inicializace ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Nastavení akce pro tlačítko Toast
+        binding.btnShowToast.setOnClickListener {
+            val toast = Toast.makeText(this, "Nazdar - mám hlad", Toast.LENGTH_LONG)
+            toast.show()
         }
-    }
-}
+
+        // Nastavení akce pro tlačítko Snackbar
+        binding.sbtShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "já jsem Snackbar", Snackbar.LENGTH_LONG)
+               // Nastavení akce pro Snakebar
+                .setDuration(7000)
+                .setAction("zavřít") {
+                    Toast.makeText(this, "zavírám Snackbar", Toast.LENGTH_SHORT).show()
+                }
+
+            .show()
+        }
+
+    }}
