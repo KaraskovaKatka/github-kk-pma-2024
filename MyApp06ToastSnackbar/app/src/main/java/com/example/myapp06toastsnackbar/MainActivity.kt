614b046b3494a871c92ef4d9c9c258ea8564c313
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-10-17 11:21:56 +0200

    #6

--- MyApp06ToastSnackbar/app/src/main/java/com/example/myapp06toastsnackbar/MainActivity.kt
+++ MyApp06ToastSnackbar/app/src/main/java/com/example/myapp06toastsnackbar/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.myapp06toastsnackbar
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp06toastsnackbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        //inicializace ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // nastavení akce kliknutí na tlačítko
+        binding.btnShowToast.setOnClickListener {
+            val toast = Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show()
+            toast.show ()
+            
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
