219345bd9f508f64883badcd5efe8b7de58f3cbb
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-10-24 13:32:23 +0200

    #7

--- MyApp07Fragments/app/src/main/java/com/example/myapp07fragments/MainActivity.kt
+++ MyApp07Fragments/app/src/main/java/com/example/myapp07fragments/MainActivity.kt
@@ -5,13 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp07fragments.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-setContentView(R.layout.activity_main))
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+
+setContentView(R.layout.activity_main)
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+            val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.FragmentContainer, fragment)
+        fragmentTransition.commit()
+
 
     }
 }
