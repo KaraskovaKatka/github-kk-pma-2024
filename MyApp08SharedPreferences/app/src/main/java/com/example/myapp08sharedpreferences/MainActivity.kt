48b035ade4d56f8cc38335a08e0ca2382df61f53
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-10-24 15:24:20 +0200

    #8

--- MyApp08SharedPreferences/app/src/main/java/com/example/myapp08sharedpreferences/MainActivity.kt
+++ MyApp08SharedPreferences/app/src/main/java/com/example/myapp08sharedpreferences/MainActivity.kt
@@ -1,23 +1,69 @@
+package com.example.myapp009asharedpreferences
 
-import android.R
-import android.content.SharedPreferences
+import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import com.example.myapp08sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var editTextName: EditText? = null
-    private var editTextAge: EditText? = null
-    private var checkBoxAdult: CheckBox? = null
-    private var sharedPreferences: SharedPreferences? = null
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        // Inicializace bindingu
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Přístup k SharedPreferences
+        val sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        // Funkcionalita pro uložení dat
+        binding.buttonSave.setOnClickListener {
+            val name = binding.editTextName.text.toString()
+            val ageString = binding.editTextAge.text.toString().trim()
+
+            if (ageString.isBlank()) {
+                Toast.makeText(this, "Hele, vyplň věk...", Toast.LENGTH_SHORT).show()
+            } else {
+                val age = ageString.toInt()
+                val isAdult = binding.checkBoxAdult.isChecked
+                if ((age < 18 && isAdult) || (age >= 18 && !isAdult)) {
+                    Toast.makeText(this, "Kecáš, takže nic ukládat nebudu", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Jasně, ukládám...", Toast.LENGTH_SHORT).show()
+                    editor.apply {
+                        putString("name", name)
+                        putInt("age", age)
+                        putBoolean("isAdult", isAdult)
+                        apply()
+                    }
+                }
+            }
+        }
+
+
+        // Funkcionalita pro načtení dat
+        binding.buttonLoad.setOnClickListener {
+            val name = sharedPref.getString("name", null)
+            val age = sharedPref.getInt("age", 0)
+            val isAdult = sharedPref.getBoolean("isAdult", false)
+
+            binding.editTextName.setText(name)
+            binding.editTextAge.setText(age.toString())
+            binding.checkBoxAdult.isChecked = isAdult
+        }
+
+
+
 
     }
-
 }
