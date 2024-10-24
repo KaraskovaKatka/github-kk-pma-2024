a467c6d841ac078072a15c2f9a25afafd5f9ba7a
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-10-24 13:18:47 +0200

    #7

--- MyApp07aFragments/app/src/main/java/com/example/myapp07afragments/Fragment1.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapp07afragments;
-
-import android.app.Activity;
-
-public class Fragment1 extends Activity {
-}
--- MyApp07aFragments/app/src/main/java/com/example/myapp07afragments/Fragment2.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapp07afragments;
-
-import android.app.Activity;
-
-public class Fragment2 extends Activity {
-}
