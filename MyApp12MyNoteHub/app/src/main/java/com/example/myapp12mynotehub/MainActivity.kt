ad4abcf875e3fe821181627760bfc3ac10d92fd3
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-11-14 14:45:24 +0100

    #12

--- MyApp12MyNoteHub/app/src/main/java/com/example/myapp12mynotehub/MainActivity.kt
+++ MyApp12MyNoteHub/app/src/main/java/com/example/myapp12mynotehub/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp12mynotehub.databinding.ActivityMainBinding
 
 
@@ -12,12 +13,18 @@ import com.example.myapp12mynotehub.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var NoteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //inicializace RecyclerView
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        NoteAdapter = NoteAdapter(emptyList())
+        binding.recyclerView.adapter = NoteAdapter
 
 
     }
