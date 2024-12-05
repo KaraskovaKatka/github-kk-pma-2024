a7db75d90f07f695f7084e066e4d0d5d5c9069cd
KaraskovaKatka <katerina.karaskova@tul.cz>
2024-12-05 19:04:52 +0100

    #12

--- MyApp015aMyNoteHub/app/src/main/java/com/example/myapp015amynotehub/models/NoteTagCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp015amynotehub.models
-
-import androidx.room.Entity
-
-@Entity(tableName = "note_tag_cross_ref", primaryKeys = ["noteId", "tagId"])
-data class NoteTagCrossRef(
-    val noteId: Int,
-    val tagId: Int
-)
--- MyApp12MyNoteHub/app/src/main/java/com/example/myapp12mynotehub/NoteTagCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp12mynotehub
-
-import androidx.room.Entity
-
-@Entity(tableName = "note_tag_cross_ref", primaryKeys = ["noteId", "tagId"])
-data class NoteTagCrossRef(
-    val noteId: Int,
-    val tagId: Int
-)
